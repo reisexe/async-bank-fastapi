1a63c3d08ce457b1d1f75ecdd6cef6d98b499412
Leonardo <leonardommiv@gmail.com>
2026-05-06 23:55:10 -0300

    debug: find python path

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/render/project/python/bin/python3 -m alembic upgrade head
-/opt/render/project/python/bin/python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
+which python3
+which pip
+pip show alembic
+python3 -m alembic upgrade head
+python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
