218d09b5078241a38ac7c927e9563ead6363a0c4
Leonardo <leonardommiv@gmail.com>
2026-05-06 23:39:13 -0300

    fix: use render python path

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-python3 -m alembic upgrade head
-python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
+/opt/render/project/python/bin/python3 -m alembic upgrade head
+/opt/render/project/python/bin/python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
