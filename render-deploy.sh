1d9f6893ad19b5abe98a24c1f0ddf1616766782e
Leonardo <leonardommiv@gmail.com>
2026-05-06 23:19:28 -0300

    fix: use venv in deploy script

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-pip install -r requirements.txt --break-system-packages
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 python3 -m alembic upgrade head
 python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
