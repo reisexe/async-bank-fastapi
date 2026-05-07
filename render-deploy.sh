3d23d328b3caa5b1c09e7e45d14ced2077d0b4dd
Leonardo <leonardommiv@gmail.com>
2026-05-06 23:59:43 -0300

    fix: use absolute python and pip paths

--- render-deploy.sh
+++ render-deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-which python3
-which pip
-pip show alembic
-python3 -m alembic upgrade head
-python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
+/usr/bin/python3 -m alembic upgrade head
+/usr/bin/python3 -m uvicorn src.main:app --host 0.0.0.0 --port $PORT
